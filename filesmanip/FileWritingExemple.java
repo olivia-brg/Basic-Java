35c6e247af0643fd47e0ed4c8b6406a66de08a2c
unknown <olivia.bergaglia@gmail.com>
2024-08-09 23:23:07 +0200

    je sais pas

--- filesmanip/FileWritingExemple.java
+++ filesmanip/FileWritingExemple.java
@@ -9,10 +9,10 @@ public class FileWritingExemple {
         BufferedWriter writer = null;
 
         try {
-            // Création d'un FileWriter pour écrire dans un fichier
-            FileWriter fileWriter = new FileWriter("../filesmanip/test.txt", false);
+            // Création d'un FileWriter qui va préparer le fichier au chemin qui lui est donné en argument ;
+            FileWriter fileWriter = new FileWriter("../filesmanip/test.txt", false); // true permet d'écrire à la suite
 
-            // Création d'un BufferedWriter qui utilise le FileWriter
+            // Création d'un BufferedWriter qui va utiliser le “fileWrite” pour écrire dans le fichier.
             writer = new BufferedWriter(fileWriter);
 
             // Ajout d'un texte à notre fichier
